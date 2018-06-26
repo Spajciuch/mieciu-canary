4ac090a3d785d2024ddfd555187b5ab4615e439f
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-06-26 20:53:38 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args, config) => {
     .addField("Link", "https://discordapp.com/api/oauth2/authorize?client_id=461233419012276234&permissions=8&scope=bot")
     .addField("Uprawnienia", "*Administarora")
     .setColor(config.embed_color)
-    message.channel.send("**"+message.authot.username +"** Sprawdź DM'y")
+    message.channel.send("**"+message.author.username +"** Sprawdź DM'y")
     message.author.send({embed})
   }
 module.exports.help = {
